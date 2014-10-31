697fc1a9384ee62ab31aef21666b2be74bfd1cc0
Elalasota <elcialas@gmail.com>
2014-10-31 01:26:10 +0100

    Pierwsze kroki

---
+++ skrypt.js
@@ -0,0 +1,6 @@
+$(document).ready(
+
+)
+var $czeko= $("<p><span>Czekolada </span>(z nahuatl xocolatl – gorzka woda) – wyrób cukierniczy sporządzany z miazgi kakaowej, tłuszczu kakaowego (masło kakaowe), środka słodzącego i innych dodatków.</p>");
+$("#tresc").prepend($czeko);
+$("#tresc").prepend($("div#tresc h3:first"));
